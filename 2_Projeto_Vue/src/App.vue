6d81328b9b315cb5bfaab3157778de1153656005
Davner Firmino Cavalcante <davner23@gmail.com>
2022-09-01 00:14:59 -0400

    📓Aula: Criando componentes no Vue - 31/08/2022

--- 2_Projeto_Vue/src/App.vue
+++ 2_Projeto_Vue/src/App.vue
@@ -1,17 +1,13 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
-import Frase1 from "./components/Frase1.vue";
+<script>
+import PrimeiroComponente from "./components/PrimeiroComponente.vue";
+export default {
+ name: "App",
+ components: { PrimeiroComponente },
+};
 </script>
 
+<style></style>
+
 <template>
- <div>
-  <h1 class="text-3xl flex justify-center uppercase font-bold bg-blue-800 text-white">Projeto Vue.js</h1>
- </div>
- <Frase1/>
+ <PrimeiroComponente />
 </template>
-
-<style scoped>
-
-</style>
---
+++ 2_Projeto_Vue/src/components/PrimeiroComponente.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+ name: "PrimeiroComponente",
+};
+</script>
+
+<template>
+ <h3 class="flex justify-center text-5xl uppercase font-bold">Vamos estudar Vue!!</h3>
+</template>
