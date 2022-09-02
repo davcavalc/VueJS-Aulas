50ce4dc5a76fb3c67f3ba21844992f562e7bf813
Davner Firmino Cavalcante <davner23@gmail.com>
2022-09-02 01:03:51 -0400

    📓Aula: Dados em componetes - 02/09/2022

--- 2_Projeto_Vue/src/App.vue
+++ 2_Projeto_Vue/src/App.vue
@@ -3,11 +3,23 @@ import PrimeiroComponente from "./components/PrimeiroComponente.vue";
 export default {
  name: "App",
  components: { PrimeiroComponente },
+ data() {
+  return {
+   nome: "Fulano de Tal",
+   profissao: "programador",
+  };
+ },
 };
 </script>
 
 <style></style>
 
 <template>
- <PrimeiroComponente />
+ <div>
+  <PrimeiroComponente />
+  <p class="flex justify-center text-2xl">
+   Meu nome é<span class="text-blue-500 font-bold ml-1 mr-1">{{ nome }}</span> e eu
+   trabalho como {{ profissao }}.
+  </p>
+ </div>
 </template>
--- 2_Projeto_Vue/src/components/PrimeiroComponente.vue
+++ 2_Projeto_Vue/src/components/PrimeiroComponente.vue
@@ -5,5 +5,5 @@ export default {
 </script>
 
 <template>
- <h3 class="flex justify-center text-5xl uppercase font-bold">Vamos estudar Vue!!</h3>
+ <h3 class="flex justify-center text-5xl uppercase font-bold bg-amber-400">Vamos estudar Vue!!</h3>
 </template>
